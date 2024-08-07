9ec9b66ed74e7270c8cbd651ef205e756d7393e9
arizzen <a1.arizzen@gmail.com>
2024-08-07 06:32:09 -0700

    s

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,11 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-	  if (task && task2) {
-		  return [task, task2];
-	  }
+    const task = true; // Block-scoped variable
+    const task2 = false; // Block-scoped variable
   }
 
   return [task, task2];
