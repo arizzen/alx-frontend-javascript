4ffa2330a6708d81edab351ffc83ef387d7db039
arizzen <a1.arizzen@gmail.com>
2024-08-08 09:28:57 -0700

    10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  if (array && array.length > 0) {
-    for (const [idx, value] of array.entries()) {
-      array[idx] = appendString + value;
-    }
+  for (const [idx, value] of array.entries()) {
+    array[idx] = appendString + value;
   }
 
   return array;
